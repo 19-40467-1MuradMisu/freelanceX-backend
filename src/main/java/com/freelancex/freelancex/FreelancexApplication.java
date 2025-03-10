698faad8463e2bb60d528624e6bba1a4d813fa91
noman ashraf <naumanashraf30@gmail.com>
2025-03-10 02:28:48 +0200

    Initial commit

---
+++ src/main/java/com/freelancex/freelancex/FreelancexApplication.java
@@ -0,0 +1,13 @@
+package com.freelancex.freelancex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancexApplication.class, args);
+	}
+
+}
