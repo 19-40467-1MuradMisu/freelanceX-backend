5f1f252188742dee2f80baaa4ab15a78b90bbec0
Sulaiman Javed <sulaimanjaved2000@gmail.com>
2025-05-04 22:43:15 +0300

    Added to remaining

--- UserService/src/main/java/com/freelancex/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/freelancex/userservice/UserServiceApplication.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -22,6 +23,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 )
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
