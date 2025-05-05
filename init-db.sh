7eb9c3349ef488ce38013474f24ffe56cdbbe559
anjish <40964308+anji5h@users.noreply.github.com>
2025-05-05 13:28:53 +0300

    added docker compose and updated application property

--- init-db.sh
+++ init-db.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE user_service;
-    CREATE DATABASE job_service;
-    CREATE DATABASE bid_service;
-    CREATE DATABASE payment_service;
-    CREATE DATABASE notification_service;
-    CREATE DATABASE rating_service;
-    CREATE DATABASE skill_service;
+psql -v ON_ERROR_STOP=1 --username "postgres" <<EOSQL
+CREATE DATABASE user_service;
+CREATE DATABASE job_service;
+CREATE DATABASE bidding_service;
+CREATE DATABASE payment_service;
+CREATE DATABASE notification_service;
+CREATE DATABASE rating_service;
+CREATE DATABASE skill_service;
+CREATE DATABASE gateway_service;
 EOSQL
