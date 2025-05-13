3501aef9f6ee1eb2434e883a8c6210e44e5a2515
muradmisu663@gmail.com <muradmisu663@gmail.com>
2025-05-14 01:04:53 +0300

    Edit profile completed

--- UserService/src/main/java/com/freelancex/userservice/controller/UserController.java
+++ UserService/src/main/java/com/freelancex/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.freelancex.userservice.controller;
 
+import com.freelancex.userservice.dtos.api.UpdateUserRequest;
 import com.freelancex.userservice.enums.UserRole;
 import com.freelancex.userservice.model.User;
 import com.freelancex.userservice.service.UserService;
@@ -26,4 +27,13 @@ public class UserController {
         User user = userService.getUserById(UUID.fromString(userId));
         return ResponseEntity.ok(user);
     }
+
+    
+    @PatchMapping("/update-profile")
+    public ResponseEntity<Void> updateUserProfile(@RequestBody UpdateUserRequest request,
+                                                 @RequestHeader("X-User-Id") String userId) {
+        userService.updateUser(UUID.fromString(userId), request);
+        return ResponseEntity.ok().build();
+    }
+    
 }
